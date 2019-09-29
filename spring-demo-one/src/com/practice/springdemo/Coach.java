cccea5ffc1bf99f6adc0863cf7d9cfa71814f12f
Nikhil Raheja <nick170294@gmail.com>
2019-09-30 00:53:00 +0530

    learning spring, initial commit

---
+++ spring-demo-one/src/com/practice/springdemo/Coach.java
@@ -0,0 +1,6 @@
+package com.practice.springdemo;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+}
---
+++ spring-demo-one/src/com/practice/springdemo/TennisCoach.java
@@ -0,0 +1,10 @@
+package com.practice.springdemo;
+
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Perform variations of plank for 30 minutes";
+	}
+
+}
