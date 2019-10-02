9cd9d6965f47bb64d6f62effb35f919ff81f9af9
Nikhil Raheja <nick170294@gmail.com>
2019-10-03 01:28:16 +0530

    Dependency Injection: Setter Injection

--- spring-demo-one/src/com/practice/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/com/practice/springdemo/SetterDemoApp.java
@@ -2,21 +2,20 @@ package com.practice.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class MyApp {
+public class SetterDemoApp {
 
 	public static void main(String[] args) {
 		// load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		// retrieve bean from spring container
-		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach coach = context.getBean("myTennisCoach", Coach.class);
 
-		// call methods on the bean
-		System.out.println(theCoach.getDailyWorkout());
+		// call methods on bean
+		System.out.println(coach.getDailyWorkout());
 
-		// let's call our new method for fortunes
-		System.out.println(theCoach.getDailyFortune());
-		
+		System.out.println(coach.getDailyFortune());
+
 		// close the context
 		context.close();
 	}
--- spring-demo-one/src/com/practice/springdemo/TennisCoach.java
+++ spring-demo-one/src/com/practice/springdemo/TennisCoach.java
@@ -2,6 +2,17 @@ package com.practice.springdemo;
 
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+
+	public TennisCoach() {
+		System.out.println("Tennis coach: Inside no-arg constructor");
+	}
+
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("Tennis Coach: inside setter method - setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Perform variations of plank for 30 minutes";
@@ -9,8 +20,7 @@ public class TennisCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
