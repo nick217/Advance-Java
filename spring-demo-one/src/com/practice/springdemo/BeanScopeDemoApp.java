52b394d10dfc68cbb12ae26e6420ae40196a01de
Nikhil Raheja <nick170294@gmail.com>
2019-10-05 19:37:08 +0530

    Bean Scopes: Singleton and Prototype

---
+++ spring-demo-one/src/com/practice/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,30 @@
+package com.practice.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+		// check if these are the same beans
+		boolean result = (theCoach == alphaCoach);
+
+		// print the result
+		System.out.println("Pointing to the same object: " + result);
+
+		System.out.println("Memory location for theCoach: " + theCoach);
+
+		System.out.println("Memory location for alpha coach: " + alphaCoach);
+
+		// close the context
+		context.close();
+	}
+}
