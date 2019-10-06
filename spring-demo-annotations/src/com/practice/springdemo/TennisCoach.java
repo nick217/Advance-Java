1d7b840248966cf843339669dda5ac3cbfa25383
Nikhil Raheja <nick170294@gmail.com>
2019-10-07 01:25:11 +0530

    Filed Injection with Java Annotations

--- spring-demo-annotations/src/com/practice/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/practice/springdemo/TennisCoach.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 @Component // this will now use the default bean-id.
 public class TennisCoach implements Coach {
 
+	@Autowired // field Injection
 	private FortuneService fortuneService;
 
 	// add default constructor
@@ -14,17 +15,30 @@ public class TennisCoach implements Coach {
 		System.out.println(">> Tennis Coach: Inside default constructor");
 	}
 	/*
-	 * @Autowired public TennisCoach(FortuneService fS) { fortuneService = fS; }
+	 * @Autowired // constructor Injection 
+	 * public TennisCoach(FortuneService fS)
+	 * { fortuneService = fS; }
 	 */
 
-	// add setter method
-	@Autowired
-	public void setFortuneService(FortuneService fS) {
+	/*
+	 * // add setter method
+	 * 
+	 * @Autowired // setter Injection 
+	 * public void setFortuneService(FortuneService fS) {
+	 * System.out.println(">> TennisCoach: Inside setFortuneService()");
+	 * fortuneService = fS;
+	 * }
+	 */
+
+	/*
+	@Autowired // field Injection
+	public void doSomeStuff(FortuneService fS) {
 		System.out.println(">> TennisCoach: Inside setFortuneService()");
 		fortuneService = fS;
 
 	}
-
+	*/
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
