134e66821ce0089479eaf8a70eb5d5d203c85491
Nikhil Raheja <nick170294@gmail.com>
2019-10-20 01:26:41 +0530

    Spring MVC, Data Binding, Annotations: @RequestMapping, @ModelAttribute

---
+++ spring-mvc-demo/src/com/practice/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,64 @@
+package com.practice.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/hello") // adds parent level mapping
+public class HelloWorldController {
+
+	// need a controller method to show the initial form
+	@RequestMapping("/showForm") // this mapping is relative to the parent
+									// mapping
+	public String showForm() {
+		return "helloworld-form";
+	}
+
+	// need a controller method to process the form
+	@RequestMapping("/processForm") // this mapping is relative to the parent
+									// mapping
+	public String processForm() {
+		return "helloworld";
+	}
+
+	// new controller method to read form data and data
+	// to the model
+	@RequestMapping("/processFormVersionTwo") // this mapping is relative to the
+												// parent mapping
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+
+		// create the message
+		String result = "Yo! " + theName;
+
+		// add message to the model
+		model.addAttribute("message", result);
+
+		return "helloworld";
+	}
+
+	@RequestMapping("/processFormVersionThree") // this mapping is relative to
+												// the parent mapping
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+
+		// create the message
+		String result = "Hi there from v3! " + theName;
+
+		// add message to the model
+		model.addAttribute("message", result);
+
+		return "helloworld";
+	}
+}
