a9a4b66dceac4752f1fd79f301e3221134c26c27
Nikhil Raheja <nick170294@gmail.com>
2019-10-29 20:30:29 +0530

    reading and querying data using hibernate

---
+++ hibernate-tutorial/src/com/practice/hibernate/demo/QueryStudentDemo.java
@@ -0,0 +1,52 @@
+package com.practice.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.practice.hibernate.demo.entity.Student;
+
+public class QueryStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// query student table
+			List<Student> students = session.createQuery("from Student").getResultList();
+			// name of the table as in code, i.e. the name of the class, not the table name in Db, display the students
+			displayStudents(students);
+
+			// query students: lastName = 'Seinfeld'
+			students = session.createQuery("from Student s where s.lastName='Seinfeld'").getResultList(); //table and column name as in code, not as in Db.
+			
+			
+			// display the students
+			displayStudents(students);
+			// commit the transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+		} finally {
+			factory.close();
+		}
+	}
+
+	private static void displayStudents(List<Student> students) {
+		for (Student student : students) {
+			System.out.println(student);
+		}
+	}
+
+}
---
+++ hibernate-tutorial/src/com/practice/hibernate/demo/ReadStudentDemo.java
@@ -0,0 +1,58 @@
+package com.practice.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.practice.hibernate.demo.entity.Student;
+
+public class ReadStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// create a student object
+			System.out.println("Creating new student object...");
+			Student tempStudent = new Student("Cosmo", "Kramer", "cosmokramer@comic.com");
+
+			// start a transaction
+			session.beginTransaction();
+
+			// save the student object
+			System.out.println("Saving the object...");
+			System.out.println(tempStudent);
+			session.save(tempStudent);
+
+			// commit the transaction
+			session.getTransaction().commit();
+
+			// find out the student's id: primary key
+			System.out.println("Saved Student Generated Id: " + tempStudent.getId());
+
+			// now get the new session and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+
+			// retrieve student based on id: primary key
+			System.out.println("\n Getting student with id: " + tempStudent.getId());
+
+			Student myStudent = session.get(Student.class, tempStudent.getId());
+
+			System.out.println("Get complete: " + myStudent);
+
+			// commit the transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+		} finally {
+			factory.close();
+		}
+	}
+
+}
