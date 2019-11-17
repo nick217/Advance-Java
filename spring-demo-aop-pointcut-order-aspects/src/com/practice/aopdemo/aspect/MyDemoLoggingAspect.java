dd1920d734c16872f2a93d7fec1b2a789d3a955f
Nikhil Raheja <nick170294@gmail.com>
2019-11-17 23:19:07 +0530

    ordering AOP aspects

---
+++ spring-demo-aop-pointcut-order-aspects/src/com/practice/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,17 @@
+package com.practice.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+	@Before("com.practice.aopdemo.aspect.AopExpressionsUtil.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n=====>>> Executing @Before advice on method");
+	}
+}
